b0cb2655856fcf3d98286b2d42dea8fc9218d81b
Astral890 <enac7125@outlook.com>
2024-07-22 19:46:49 -0600

    Inclusion de lo de Jesus

--- app/index.js
+++ app/index.js
@@ -27,7 +27,7 @@ server.post("/api/login",authentication.login)
 server.post("/api/registrer",authentication.registrer)
  
 //Conexion con la base de datos
-import  db from "mysql2";
+import db from "mysql2";
 const conn=db.createConnection({
 host: "localhost",
 user: "root",          // Remplazar con tu nombre de usuario
@@ -52,3 +52,38 @@ server.use(bodyParser.json());
 server.use(cors());
 
 //Submit en la db
+server.post("/postmascota", (req, res) => {
+    const { NombreA, EspecieA, RazaA, EdadA, PesoA, SexoA, DescripcionColorA } = req.body;
+
+    conn.query("SELECT * FROM animales WHERE fk_usuario = 3;", (err, trow) => {
+        if (err) {
+            console.log("Error fetching data", err);
+            return res.status(500).send("Error fetching data");
+        }
+
+       const repetido= trow.some((row) => {
+            return row.nombre_animal == NombreA && row.especie_a == EspecieA && row.raza_a == RazaA && row.edad == EdadA && row.peso_a == PesoA && row.sexo_a == SexoA && row.info_adicional_a == DescripcionColorA 
+                
+        });
+
+        if (repetido) {
+            console.log("Nombre repetido");
+            return res.status(400).send("Repetido todo");
+        } else{const insertQuery = `
+                INSERT INTO animales(nombre_animal, especie_a, raza_a, edad, peso_a, sexo_a, info_adicional_a, fk_usuario)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+            `;
+            const insertValues = [NombreA, EspecieA, RazaA, EdadA, PesoA, SexoA, DescripcionColorA, 3];
+
+            conn.query(insertQuery, insertValues, (err, results) => {
+                if (err) {
+                    console.log("Error inserting data", err);
+                    return res.status(500).send("Error inserting data");
+                }
+
+                console.log("Data inserted successfully");
+                return res.redirect("/");
+            });
+        }
+    });
+});
