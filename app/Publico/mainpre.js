c0ad0b228b1f4641e3296b034ed8e8237015dc54
Astral890 <enac7125@outlook.com>
2024-07-30 09:53:32 -0600

    Correcion en las traduciones

--- app/Publico/mainpre.js
+++ app/Publico/mainpre.js
@@ -2,7 +2,11 @@ const boton=document.getElementById('login');
 boton.addEventListener('click',()=>{
     window.location.href="/sIn";
 })
-const botontrans=document.getElementById('trans');
-botontrans.addEventListener('click',()=>{
+const botontrans_eng=document.getElementById('trans_eng');
+botontrans_eng.addEventListener('click',()=>{
     window.location.href="/e"
+})
+const botontrans_esp=document.getElementById('trans_esp');
+botontrans_esp.addEventListener('click',()=>{
+    window.location.href="/"
 })
