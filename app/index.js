31ded4ebde2281e8688089d7d75c678a60b18a2a
pepepecas12000 <jjimenechuyin@gmail.com>
2024-07-31 00:12:50 -0600

    index citas

--- app/index.js
+++ app/index.js
@@ -24,6 +24,7 @@ server.get("/sMa",(req,res)=> res.sendFile(_dirname + "/Paginas/Registro mascota
 server.get("/store",(req,res)=> res.sendFile(_dirname + "/Paginas/Tienda/Tienda main/Tienda.html"))
 server.get("/s",(req,res)=> res.sendFile(_dirname + "/Paginas/Tienda/Productos/Productos.html"))
 server.get("/e",(req,res)=> res.sendFile(_dirname + "/Paginas/Main pre/mainpre_english.html"))
+server.get("/cita",(req,res)=> res.sendFile(_dirname + "/Paginas/Registro cita/citas.html"))
 server.get("/registromascotas",(req,res)=> res.sendFile(_dirname + "/Paginas/Registro mascota/Registro mascota.html"))
 server.post("/api/login",authentication.login)
 server.post("/api/registrer",authentication.registrer)
