ad06d2f89062701df3959033f800a9d3600dd38f
Astral890 <enac7125@outlook.com>
2024-07-31 20:35:03 -0600

    Experimento

--- app/Publico/mainpre.js
+++ app/Publico/mainpre.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded',(req,res)=>{
+    const verCookie=req.target.cookie;
+    console.log(verCookie);
+    if(verCookie){
+        window.location.href='/post'
+    }
+});
+
 const boton=document.getElementById('login');
 boton.addEventListener('click',()=>{
     window.location.href="/sIn";
@@ -10,3 +18,35 @@ const botontrans_esp=document.getElementById('trans_esp');
 botontrans_esp.addEventListener('click',()=>{
     window.location.href="/"
 })
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    Swal.fire({
+        title: "¡Tu mensaje ha sido recibido!",
+        text: "Gracias por contactarnos. Revisaremos tu consulta y te responderemos lo antes posible.",
+        icon: "success",
+        customClass: {
+        confirmButton: 'custom-button'
+         }
+    }).then((result) => {
+        if (result.isConfirmed) {
+        }
+    });
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    Swal.fire({
+        title: "¡Tu mensaje ha sido recibido!",
+        text: "Gracias por contactarnos. Revisaremos tu consulta y te responderemos lo antes posible.",
+        icon: "success",
+        customClass: {
+        confirmButton: 'custom-button'
+         }
+    }).then((result) => {
+        if (result.isConfirmed) {
+        }
+    });
+});
